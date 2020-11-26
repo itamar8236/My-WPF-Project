4ff28faf6bd06d41eab030259f585c526c35f5e9
Itamar Cohen <itamar8236@gmail.com>
2020-11-26 21:21:05 +0200

    icon

--- איתמר כהן 318558236/WpfApplication1/Classes/InformationOfShot.cs
+++ איתמר כהן 318558236/WpfApplication1/Classes/InformationOfShot.cs
@@ -1,21 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using System.Windows.Media.Media3D;
-using System.Windows.Media.Animation;
-using System.Windows.Threading;
-
-namespace WpfApplication1
+﻿namespace WpfApplication1
 {
     public class InformationOfShot
     {
